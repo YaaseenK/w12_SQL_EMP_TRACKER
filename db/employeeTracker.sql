68ab56054c09f56309db32e1b18797401623fe9c
YaaseenK <yaaseenkhan02@gmail.com>
2023-01-01 20:21:00 -0500

    finished seed

--- db/employeeTracker.sql
+++ db/employeeTracker.sql
@@ -13,7 +13,8 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -22,8 +23,10 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL, 
     manager_id INT NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    FOREIGN KEY (role_id) 
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id) 
+    REFERENCES employee(id),
     ON DELETE SET NULL
 );
 
--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,41 @@
+-- DEPARTMENT VALUES TO INSERT
 INSERT INTO department (id, departmentName)
-VALUES (D);
+VALUES ('Engineering', 'Finance', 'Legal', 'Sales');
+
+-- ROLE VALUES TO INSERT
+INSERT INTO role (title, salary, department_id)
+VALUES 
+-- engineering
+("Senior Engineer", 150000, 1),
+("Junior Engineer", 70000, 1),
+("Lead Engineer", 90000, 1),
+-- financial
+("Bookkeeping", 90000, 2),
+("Budgets and forecasting", 75000, 2),
+("Management of Taxes", 65000, 2),
+-- legal
+("Head of Legal ", 95000, 3),
+("Legal Director", 92500, 3),
+("Legal Manager", 70000, 3),
+-- sales
+("Administration", 65000, 4),
+("Operations manager", 45000, 4),
+("Success Planner", 35000, 4)
+
+-- EMPLOYEE VALUES TO INSERT
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Adam', 'Kin', 1, NULL),
+('John', 'Doe', 1, NULL),
+('Zach', 'Tyler', 1, NULL),
+('Mae', 'Whitman', 2, NULL),
+('Jack', 'DeSena', 2, NULL),
+('Makoto', 'Iwamatsu', 2, NULL),
+('Dante', 'Basco', 3, NULL),
+('Greg', 'Baldwin', 3, NULL),
+('Bradley', 'Baker', 3, NULL),
+('Jessie', 'Flower', 4, NULL),
+('Grey', 'DeListle', 4, NULL),
+('Olivia', 'Hack', 4, NULL)
+
        
